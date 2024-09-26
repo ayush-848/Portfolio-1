cfb71f27a5599ed23f02b77e00d3c0667489f2a2
Ayush Deb <ayushdeb848@gmail.com>
2024-09-26 18:54:39 +0530

    updated the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className='w-full border-t-2 border-solid text-lg border-dark font-medium dark:border-light'>
-        <Layout className='!py-6 flex items-center justify-between'>
-            <span className='text-dark dark:text-light h-full w-full p-32 xl:p-24 lg:p-16 -z-1  md:p-12 sm:p-8 flex items-center justify-between lg:flex-col lg:py-6 md:text-xs'>{new Date().getFullYear()} &copy; All Rights Reserved
+    <footer className='w-full border-t-2 border-solid text-lg border-dark font-medium dark:border-light h-1/4'>
+        <Layout className='!py-2 flex items-center justify-between'>
+            <span className='text-dark dark:text-light h-1/5 w-full p-32 xl:p-20 lg:p-16 -z-1  md:p-12 sm:p-8 flex items-center justify-between lg:flex-col lg:py-6 md:text-xs'>{new Date().getFullYear()} &copy; All Rights Reserved
             <div className='flex items-center text-dark dark:text-light lg:py-2'>
                 Build by &nbsp;<Link href="/" className='underline underline-offset-4'>Ayush Deb</Link>
             </div>
